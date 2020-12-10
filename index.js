15ec1ad2def06f245551d0aed1483d04b6fd9f37
Jéssica Arruda <sicarruda@gmail.com>
2020-12-10 14:53:51 -0300

    CoCriação da funsção reset, correção das funções de conta, exibição do resultado da conta na tela

--- index.js
+++ index.js
@@ -1,107 +1,82 @@
 var listaNumeros = [];
 var operadorNumerico = null;
 var listaNumerosConvertidos = [];
-var resultado;
+var conta;
+var exibeResposta = document.getElementById("resposta");
 
-function adicionarNumeroLista(numero) {
-  console.log(numero)
-    listaNumeros.push(numero);
-  
+//Reseta todos os parametros para que a conta comece do zero
+function resetarConta() {
+  listaNumeros = [];
+  conta = 0;
+  listaNumerosConvertidos = [];
+  operadorNumerico = null;
+  exibeResposta.innerHTML = 0;
 }
 
+//Essa função adiciona os números clicados pelo usuário em um array.
+function adicionarNumeroLista(numero) {
+  listaNumeros.push(numero);
+}
+/*A função convertendoNumeroLista converte os números unitarios clicados pelo usuário em um 
+unico número guardando esse valor em outro array. */
 function convertendoNumeroLista() {
   var stringElementoNumerico;
   var elementoNumerico;
-  if (listaNumerosConvertidos.length == 0) {
-    stringElementoNumerico = listaNumeros.join("");
-    elementoNumerico = parseInt(stringElementoNumerico);
-    return elementoNumerico;
-  } else {
-    stringElementoNumerico = listaNumeros.join("")[1];
-    elementoNumerico = parseInt(stringElementoNumerico);
-    return elementoNumerico;
-  }
+  stringElementoNumerico = listaNumeros.join("");
+  elementoNumerico = parseInt(stringElementoNumerico);
+  listaNumeros = [];
+  return elementoNumerico;
 }
-
+/*A função adicionarOperador adiciona a conta o operador selecionado pelo usuário, chama a 
+função convertendoNumeroLista para obter a as duas primeiras partes da operação.*/
 function adicionarOperadores(operador) {
-  console.log(operador)
+  listaNumerosConvertidos.push(convertendoNumeroLista());
+  fazerConta(listaNumerosConvertidos);
   if (operador === "somar") {
     operadorNumerico = operador;
-    listaNumerosConvertidos = [convertendoNumeroLista()];
   } else if (operador === "subtrair") {
     operadorNumerico = operador;
-    listaNumerosConvertidos = [convertendoNumeroLista()];
   } else if (operador === "dividir") {
     operadorNumerico = operador;
-    listaNumerosConvertidos = [convertendoNumeroLista()];
-
   } else if (operador === "multiplicar") {
     operadorNumerico = operador;
-    listaNumerosConvertidos = [convertendoNumeroLista()];
-
-  } else if (operador === "true") {
-    resultado = true;
-    fazerConta(listaNumerosConvertidos)
-  } else {
+  } else if (operador === "resultado") {
     operadorNumerico = null;
   }
 }
-
+/*A função fazerConta encerra o processo chamando novamente a função convertendoNumeroLista
+para obter a segunda seguencia numerica e poder fazer a respeciva conta solicitada pelo
+usuário.*/
 function fazerConta(arrayNumeros) {
-  let conta;
-  if (resultado == true) {
-    if (operadorNumerico === "soma") {
+  if (listaNumerosConvertidos.length == 2) {
+    if (operadorNumerico === "somar") {
       conta = arrayNumeros[0] + arrayNumeros[1];
-      listaNumerosConvertidos.slice(0, 2);
-      resultado = false;
+      exibeResposta.innerHTML =
+        arrayNumeros[0] + " + " + arrayNumeros[1] + " = " + conta;
+      listaNumerosConvertidos.splice(0, 2);
+      console.log(conta);
+      return conta;
     } else if (operadorNumerico === "subtrair") {
       conta = arrayNumeros[0] - arrayNumeros[1];
-      listaNumerosConvertidos.slice(0, 2);
-      resultado = false;
+      exibeResposta.innerHTML =
+        arrayNumeros[0] + " - " + arrayNumeros[1] + " = " + conta;
+      listaNumerosConvertidos.splice(0, 2);
+      console.log(conta);
+      return conta;
     } else if (operadorNumerico === "dividir") {
       conta = arrayNumeros[0] / arrayNumeros[1];
-      listaNumerosConvertidos.slice(0, 2);
-      resultado = false;
+      exibeResposta.innerHTML =
+        arrayNumeros[0] + " ÷ " + arrayNumeros[1] + " = " + conta;
+      listaNumerosConvertidos.splice(0, 2);
+      console.log(conta);
+      return conta;
     } else if (operadorNumerico === "multiplicar") {
       conta = arrayNumeros[0] * arrayNumeros[1];
-      listaNumerosConvertidos.slice(0, 2);
-      resultado = false;
-    }
-  } else {
-    if (operadorNumerico === "soma") {
-      listaNumerosConvertidos.slice(0, 2, conta);
-      conta = arrayNumeros[0] / arrayNumeros[1];
-      listaNumerosConvertidos.slice(0, 2, conta);
-    } else if (operadorNumerico === "multiplicar") {
-      conta = arrayNumeros[0] * arrayNumeros[1];
-      listaNumerosConvertidos.slice(0, 2, conta);
+      exibeResposta.innerHTML =
+        arrayNumeros[0] + " x " + arrayNumeros[1] + " = " + conta;
+      listaNumerosConvertidos.splice(0, 2);
+      console.log(conta);
+      return conta;
     }
   }
 }
-
-
-// function criandoElementoNumerico(array) {
-//   if (fimDaLista == true) {
-//     var stringElementoNumerico = array.join("");
-//     var elementoNumerico = parseInt(stringElementoNumerico);
-//     fimDaLista = false;
-//     console.log(elementoNumerico);
-//     return elementoNumerico;
-//   }
-// }
-
-// function adicionarOperadores(operador) {
-//   if (operador === "somar") {
-//     operadorNumerico = operador;
-//   } else if (operador === "subtrair") {
-//     operadorNumerico = operador;
-//   } else if (operador === "dividir") {
-//     operadorNumerico = operador;
-//   } else if (operador === "multiplicar") {
-//     operadorNumerico = operador;
-//   } else {
-//     operadorNumerico = null;
-//   }
-// }
-
-// console.log(elementoNumerico);
